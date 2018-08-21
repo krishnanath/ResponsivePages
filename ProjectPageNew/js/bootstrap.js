733256d31c16133d554a1e4969fd6246a76a980e
krishnanath <isabella0484@gmail.com>
2018-08-21 22:22:15 +0530

    smooth scrolling

--- ProjectPageNew/js/bootstrap.js
+++ ProjectPageNew/js/bootstrap.js
@@ -2316,3 +2316,21 @@ if (typeof jQuery === 'undefined') {
   
   }(jQuery);
   
+  // Smooth Scrolling
+$('.cf a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
