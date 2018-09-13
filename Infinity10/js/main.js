2e5039ec2f3e3ddddc36527f7c389ea03e58117d
krishnanath <isabella0484@gmail.com>
2018-09-13 21:53:13 +0530

    error call

--- Infinity10/js/main.js
+++ Infinity10/js/main.js
@@ -424,3 +424,30 @@
  
 
 })(jQuery);
+
+//remove
+/* Initialize
+	* ------------------------------------------------------ */
+	(function ssInit() {
+
+		ssPreloader();
+		ssFitVids();
+		ssMasonryFolio();
+		ssLightGallery();
+		ssFlexSlider();
+		ssOwlCarousel();
+		ssMenuOnScrolldown();
+		ssOffCanvas();
+		ssSmoothScroll();
+		ssPlaceholder();
+		ssAlertBoxes();
+		ssAnimations();
+		ssIntroAnimation();		
+		ssContactForm();
+		ssAjaxChimp();
+		ssBackToTop();
+
+	})();
+ 
+
+})(jQuery);
