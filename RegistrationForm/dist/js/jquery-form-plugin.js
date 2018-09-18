2254878e91a95216d35b265c44483ba2c2adacd0
krishnanath <isabella0484@gmail.com>
2018-09-18 21:26:04 +0530

    registration form

---
+++ RegistrationForm/dist/js/jquery-form-plugin.js
@@ -0,0 +1,81 @@
+// $(document).ready(function(){
+//     var placeholder = null;
+//     $('input[type=text]').focus(function(){
+//       placeholder = $(this).attr("placeholder");
+//       $(this).attr("placeholder","");
+//     });
+//     $('input[type=text]').blur(function(){
+//       $(this).attr("placeholder", placeholder);
+//     });
+//   });
+
+var country_arr = new Array("Afghanistan", "Albania", "Algeria", "American Samoa", "Angola", "Anguilla", "Antartica", "Antigua and Barbuda", "Argentina", "Armenia" );
+
+// States
+var s_a = new Array();
+s_a[0]="";
+s_a[1]="Badakhshan|Badghis|Baghlan|Balkh|Bamian|Farah|Faryab|Ghazni|Ghowr|Helmand|Herat|Jowzjan|Kabol|Kandahar|Kapisa|Konar|Kondoz|Laghman|Lowgar|Nangarhar|Nimruz|Oruzgan|Paktia|Paktika|Parvan|Samangan|Sar-e Pol|Takhar|Vardak|Zabol";
+s_a[2]="Berat|Bulqize|Delvine|Devoll (Bilisht)|Diber (Peshkopi)|Durres|Elbasan|Fier|Gjirokaster|Gramsh|Has (Krume)|Kavaje|Kolonje (Erseke)|Korce|Kruje|Kucove|Kukes|Kurbin|Lezhe|Librazhd|Lushnje|Malesi e Madhe (Koplik)|Mallakaster (Ballsh)|Mat (Burrel)|Mirdite (Rreshen)|Peqin|Permet|Pogradec|Puke|Sarande|Shkoder|Skrapar (Corovode)|Tepelene|Tirane (Tirana)|Tirane (Tirana)|Tropoje (Bajram Curri)|Vlore";
+s_a[3]="Adrar|Ain Defla|Ain Temouchent|Alger|Annaba|Batna|Bechar|Bejaia|Biskra|Blida|Bordj Bou Arreridj|Bouira|Boumerdes|Chlef|Constantine|Djelfa|El Bayadh|El Oued|El Tarf|Ghardaia|Guelma|Illizi|Jijel|Khenchela|Laghouat|M'Sila|Mascara|Medea|Mila|Mostaganem|Naama|Oran|Ouargla|Oum el Bouaghi|Relizane|Saida|Setif|Sidi Bel Abbes|Skikda|Souk Ahras|Tamanghasset|Tebessa|Tiaret|Tindouf|Tipaza|Tissemsilt|Tizi Ouzou|Tlemcen";
+s_a[4]="Eastern|Manu'a|Rose Island|Swains Island|Western";
+s_a[5]="Andorra la Vella|Bengo|Benguela|Bie|Cabinda|Canillo|Cuando Cubango|Cuanza Norte|Cuanza Sul|Cunene|Encamp|Escaldes-Engordany|Huambo|Huila|La Massana|Luanda|Lunda Norte|Lunda Sul|Malanje|Moxico|Namibe|Ordino|Sant Julia de Loria|Uige|Zaire";
+s_a[6]="Anguilla";
+s_a[7]="Antartica";
+s_a[8]="Barbuda|Redonda|Saint George|Saint John|Saint Mary|Saint Paul|Saint Peter|Saint Philip";
+s_a[9]="Antartica e Islas del Atlantico Sur|Buenos Aires|Buenos Aires Capital Federal|Catamarca|Chaco|Chubut|Cordoba|Corrientes|Entre Rios|Formosa|Jujuy|La Pampa|La Rioja|Mendoza|Misiones|Neuquen|Rio Negro|Salta|San Juan|San Luis|Santa Cruz|Santa Fe|Santiago del Estero|Tierra del Fuego|Tucuman";
+s_a[10]="Aragatsotn|Ararat|Armavir|Geghark'unik'|Kotayk'|Lorri|Shirak|Syunik'|Tavush|Vayots' Dzor|Yerevan";
+// <!-- -->
+
+
+
+function populateStates( countryElementId, stateElementId ){
+	
+	var selectedCountryIndex = document.getElementById( countryElementId ).selectedIndex;
+
+	var stateElement = document.getElementById( stateElementId );
+	
+	stateElement.length=0;	// Fixed by Julian Woods
+	stateElement.options[0] = new Option('Select State','');
+	stateElement.selectedIndex = 0;
+	
+	var state_arr = s_a[selectedCountryIndex].split("|");
+	
+	for (var i=0; i<state_arr.length; i++) {
+		stateElement.options[stateElement.length] = new Option(state_arr[i],state_arr[i]);
+	}
+}
+
+function populateCountries(countryElementId, stateElementId){
+	// given the id of the <select> tag as function argument, it inserts <option> tags
+	var countryElement = document.getElementById(countryElementId);
+	countryElement.length=0;
+	countryElement.options[0] = new Option('Select Country','-1');
+	countryElement.selectedIndex = 0;
+	for (var i=0; i<country_arr.length; i++) {
+		countryElement.options[countryElement.length] = new Option(country_arr[i],country_arr[i]);
+	}
+
+	// Assigned all countries. Now assign event listener for the states.
+
+	if( stateElementId ){
+		countryElement.onchange = function(){
+			populateStates( countryElementId, stateElementId );
+		};
+	}
+}
+
+
+  $('.addmore').click(function(e) {
+      $clone = $('#country').clone();
+      $clone.attr('id', 'country' + ($('.country').length + 1));
+      $clone.insertBefore($('.button-div'));
+    
+        $clones = $('#state').clone();
+        $clones.attr('id', 'state' + ($('.state').length + 1));
+        $clones.insertBefore($('.button-div'));
+    
+    });
+
+
+  populateCountries("country", "state");
+ 
